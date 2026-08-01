a4eb43b59d32697e0f11df8ef599bd88f5083827
advita.vedanta01@gmail.com <advita.vedanta01@gmail.com>
2026-08-01 12:47:35 +0530

    updated backend

--- environments/dev/backend/dev-backend-config.tf
+++ environments/dev/backend/dev-backend-config.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-mycompany"
+    bucket         = "terraform-state-mycompany12345"
     key            = "ecs/dev/terraform.tfstate"
     region         = "ap-south-1"
     use_lockfile   = true
