d61a902cea222381f43e13fe5d34ecd24af4b612
Ganga <63787967+Ganga-dhar@users.noreply.github.com>
2026-08-28 10:01:42 +0530

    Delete IAM role and profile for ECS instances
    Removed IAM role and instance profile resources for ECS instances.

--- modules/ecs-cluster/main.tf
+++ modules/ecs-cluster/main.tf
@@ -1,47 +1,4 @@
-#############################
-# IAM Role for ECS Instances
-#############################
 
-resource "aws_iam_role" "ecs_instance" {
-
-  name = "${var.project_name}-ecs-instance-role"
-
-  assume_role_policy = jsonencode({
-
-    Version = "2012-10-17"
-
-    Statement = [
-      {
-        Effect = "Allow"
-
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-
-        Action = "sts:AssumeRole"
-      }
-    ]
-
-  })
-}
-
-
-resource "aws_iam_role_policy_attachment" "ecs_instance" {
-
-  role = aws_iam_role.ecs_instance.name
-
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
-
-}
-
-
-resource "aws_iam_instance_profile" "ecs" {
-
-  name = "${var.project_name}-ecs-profile"
-
-  role = aws_iam_role.ecs_instance.name
-
-}
 
 
 #############################
